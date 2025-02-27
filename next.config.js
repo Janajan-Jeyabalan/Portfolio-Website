4bcf5e9acfbb6ab25ee99bba42f1affc5b590443
Janajan Jeyabalan <75293182+Janajan-Jeyabalan@users.noreply.github.com>
2025-02-27 03:49:09 -0500

    Updated Config

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/janajan-portfolio',
+    assetPrefix: '/janajan-portfolio/',
+    trailingSlash: true,
+    output: 'export',
+  }
